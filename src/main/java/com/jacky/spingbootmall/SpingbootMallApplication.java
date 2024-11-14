8472b32c047b3379423e27dc01d1433d48e7541c
IEC110562 <Chen.JackYQ@inventec.com>
2024-11-14 11:36:36 +0800

    Init

---
+++ src/main/java/com/jacky/spingbootmall/SpingbootMallApplication.java
@@ -0,0 +1,13 @@
+package com.jacky.spingbootmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpingbootMallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpingbootMallApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jacky/spingbootmall/SpingbootMallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jacky.spingbootmall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpingbootMallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
