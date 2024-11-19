fc260dedf9a814ba199f771269d5bfaed264e9a0
IEC110562 <Chen.JackYQ@inventec.com>
2024-11-19 17:06:26 +0800

    商品功能 - 實作刪除商品

--- src/main/java/com/jacky/spingbootmall/dao/ProductDao.java
+++ src/main/java/com/jacky/spingbootmall/dao/ProductDao.java
@@ -8,4 +8,5 @@ public interface ProductDao {
     Product getProductById(int productId);
     Integer createProduct(ProductRequest productRequest);
     void updateProduct(int productId, ProductRequest productRequest);
+    void deleteProductById(int productId);
 }
