f967b17e003722ad2b22ac19549601ba6bf3b0e4
IEC110562 <Chen.JackYQ@inventec.com>
2024-11-21 17:05:20 +0800

    訂單功能 - 檢查User是否存在，扣除商品庫存

--- src/main/java/com/jacky/spingbootmall/dao/ProductDao.java
+++ src/main/java/com/jacky/spingbootmall/dao/ProductDao.java
@@ -13,5 +13,6 @@ public interface ProductDao {
     Product getProductById(int productId);
     Integer createProduct(ProductRequest productRequest);
     void updateProduct(int productId, ProductRequest productRequest);
+    void updateStock(int productId, int stock);
     void deleteProductById(int productId);
 }
